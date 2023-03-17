23bdbea9ee80c8432fcb75d0b96f7e873c46fe5e
Olivier <olvr.ritter@gmail.com>
2023-03-17 15:53:47 +0100

    Ajout method get df JSInstance

--- JSInstance.py
+++ JSInstance.py
@@ -12,22 +12,25 @@ class JSInstance():
         #Check that the file is an excel spreadsheet
         if datafile[-5:] == ".xlsx":
             #Get the informations on the processing times of each job
-            self._processtimes = pd.read_excel(datafile, sheet_name, index_col=0)
+            self._data = pd.read_excel(datafile, sheet_name, index_col=0)
         else:
             raise NameError("Expected a '.xlsx' file")
 
     def print(self):
         """Print the main information about the instance."""
-        if self._processtimes.empty:
+        if self._data.empty:
             print("Empty instance: Use a data file to fill the parameters")
         else:
-            print("Number of the village: {}".format(self._processtimes.index.size))
-            print("Number of machines: {}".format(self._processtimes.columns.size))
+            print("Number of the village: {}".format(self._data.index.size))
+            print("Number of machines: {}".format(self._data.columns.size))
             # Add some (local) options to display the dataframe
             with pd.option_context('display.max_rows', None,
                        'display.max_columns', None,
                        'display.precision', 3):
-                print(self._processtimes)
+                print(self._data)
+
+    def get_df(self):
+        return self._data
 
 
         
--- Matrix.py
+++ Matrix.py
@@ -3,9 +3,9 @@ import JSInstance
 
 class Matrix:
     def __init__(self, villages : JSInstance, centres : JSInstance):
-        self.rows = len(villages. index)
-        self.cols = len(centres. index)
-        self.matrix = [[0 for _ in range(len(villages. index))] for _ in range(len(centres. index))]
+        self._rows = len(villages. index)
+        self._cols = len(centres. index)
+        self._matrix = [[0 for _ in range(len(villages. index))] for _ in range(len(centres. index))]
 
     
 
@@ -13,17 +13,17 @@ class Matrix:
         matrix_str = ""
         for i in range(self.rows):
             for j in range(self.cols):
-                matrix_str += str(self.matrix[i][j]) + " "
+                matrix_str += str(self._matrix[i][j]) + " "
             matrix_str += "\n"
         return matrix_str
 
     def set_value(self, row, col, value):
-        self.matrix[row][col] = value
+        self._matrix[row][col] = value
 
     def get_value(self, row, col):
-        return self.matrix[row][col]
+        return self._matrix[row][col]
     
 
     def fillMatrix(self, villages : JSInstance, centres : JSInstance):
-        
+        for index, row in villages.
         pass
