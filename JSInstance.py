2dc5fc6eb91086bf0e5c8b6b7d5bc0443d9ad630
MonoO7 <m.nomoko7@gmail.com>
2023-03-17 15:45:16 +0100

    Changement sheet_name

--- JSInstance.py
+++ JSInstance.py
@@ -5,14 +5,14 @@ import pandas as pd
 class JSInstance():
 
     # Constructor
-    def __init__(self, datafile=""):
+    def __init__(self, datafile="", sheet_name=""):
         """
         Collect the informations from the input file to build the job-shop scheduling instance.
         """
         #Check that the file is an excel spreadsheet
         if datafile[-5:] == ".xlsx":
             #Get the informations on the processing times of each job
-            self._processtimes = pd.read_excel(datafile, sheet_name="Processing times", index_col=0)
+            self._processtimes = pd.read_excel(datafile, sheet_name, index_col=0)
         else:
             raise NameError("Expected a '.xlsx' file")
 
@@ -21,7 +21,7 @@ class JSInstance():
         if self._processtimes.empty:
             print("Empty instance: Use a data file to fill the parameters")
         else:
-            print("Number of jobs: {}".format(self._processtimes.index.size))
+            print("Number of the village: {}".format(self._processtimes.index.size))
             print("Number of machines: {}".format(self._processtimes.columns.size))
             # Add some (local) options to display the dataframe
             with pd.option_context('display.max_rows', None,
